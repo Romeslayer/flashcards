5e9520aa3fea009e88468e8c30510ae48a03c91d
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-01-31 21:03:30 -0700

    Create test to check if guess is correct

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -36,5 +36,11 @@ describe('Turn', function() {
     const card = new Card();
     const turn = new Turn('opera', card);
     expect(turn.returnCard()).to.equal(card);
-  })
+  });
+
+  it.skip('should be able to check if user\'s guess is correct', function() {
+    const card = new Card(1, 'What allows you to define a set of relate information using key-value pairs?', ['object','array','function'], 'object');
+    const turn = new Turn('object', card);
+    expect(turn.evaluateGuess()).to.equal(true);
+  });
 });
