ac89fb7b61cdd001ade07384da475e27654bf4e4
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-02-01 16:41:39 -0700

    Refactor to remove moisture

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -16,17 +16,19 @@ describe('Deck', () => {
 
   it('should hold Cards', () => {
     const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    const card2 = new Card(1, 'What allows you to define a set of related information using indecies?', ['object', 'array', 'function'], 'array');
-    const card3 = new Card(1, 'What allows you to use a block of code multiple times?', ['object', 'array', 'function'], 'function');
-    const deck = new Deck([card1, card2, card3]);
-    expect(deck.cards).to.be.deep.equal([card1, card2, card3]);
+    const card2 = new Card(2, 'What allows you to define a set of related information using indecies?', ['object', 'array', 'function'], 'array');
+    const card3 = new Card(3, 'What allows you to use a block of code multiple times?', ['object', 'array', 'function'], 'function');
+    const cards = [card1, card2, card3];
+    const deck = new Deck(cards);
+    expect(deck.cards).to.be.deep.equal(cards);
   });
 
   it('should know how many cards are in the Deck', () => {
     const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    const card2 = new Card(1, 'What allows you to define a set of related information using indecies?', ['object', 'array', 'function'], 'array');
-    const card3 = new Card(1, 'What allows you to use a block of code multiple times?', ['object', 'array', 'function'], 'function');
-    const deck = new Deck([card1, card2, card3]);
-    expect(deck.countCards()).to.be.equal(3);
+    const card2 = new Card(2, 'What allows you to define a set of related information using indecies?', ['object', 'array', 'function'], 'array');
+    const card3 = new Card(3, 'What allows you to use a block of code multiple times?', ['object', 'array', 'function'], 'function');
+    const cards = [card1, card2, card3];
+    const deck = new Deck(cards);
+    expect(deck.countCards()).to.be.equal(deck.length);
   })
 });
