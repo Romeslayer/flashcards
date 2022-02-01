7ebc8fe64b920c1ca124a4ad8248d2208630a76b
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-01-31 17:01:40 -0700

    Create first test that checks if turn is a function

---
+++ test/Turn-test.js
@@ -0,0 +1,11 @@
+const expect = require('chai').expect;
+
+const Turn = require('../src/Turn');
+
+describe('Turn', function() {
+
+  it.skip('should be a function', function() {
+    const turn = new Turn();
+    expect(Turn).to.be.a('function');
+  });
+});
