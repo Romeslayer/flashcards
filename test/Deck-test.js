4fb6683260bb6aa742fd098d82c3b47333e15d4a
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-02-01 15:33:29 -0700

    Complete tests up to holding cards

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -9,12 +9,12 @@ describe('Deck', () => {
     expect(Deck).to.be.a('function');
   });
 
-  it.skip('should be an instance of Deck', () => {
+  it('should be an instance of Deck', () => {
     const deck = new Deck();
     expect(deck).to.be.an.instanceof(Deck);
   });
 
-  it.skip('should hold Cards', () => {
+  it('should hold Cards', () => {
     const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     const card2 = new Card(1, 'What allows you to define a set of related information using indecies?', ['object', 'array', 'function'], 'array');
     const card3 = new Card(1, 'What allows you to use a block of code multiple times?', ['object', 'array', 'function'], 'function');
