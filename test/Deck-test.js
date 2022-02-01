3e843d385a7c70ab1394b134b1302ac8b3807d4d
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-02-01 15:36:44 -0700

    Create method to return amount of cards in deck

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -22,7 +22,7 @@ describe('Deck', () => {
     expect(deck.cards).to.be.deep.equal([card1, card2, card3]);
   });
 
-  it.skip('should know how many cards are in the Deck', () => {
+  it('should know how many cards are in the Deck', () => {
     const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     const card2 = new Card(1, 'What allows you to define a set of related information using indecies?', ['object', 'array', 'function'], 'array');
     const card3 = new Card(1, 'What allows you to use a block of code multiple times?', ['object', 'array', 'function'], 'function');
