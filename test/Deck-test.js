737f84d7d5c4b06dceaa3783acd50c120e00764a
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-02-01 15:17:36 -0700

    Create tests for Deck

---
+++ test/Deck-test.js
@@ -0,0 +1,32 @@
+const expect = require('chai').expect;
+const Deck = require('../src/Deck');
+const Card = require('../src/Card');
+
+describe('Deck', function() {
+
+  it.skip('should be a function', function() {
+    const deck = new Deck();
+    expect(Deck).to.be.a('function');
+  });
+
+  it.skip('should be an instance of Deck', function() {
+    const deck = new Deck();
+    expect(deck).to.be.an.instanceof(Deck);
+  });
+
+  it.skip('should hold Cards', () => {
+    const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const card2 = new Card(1, 'What allows you to define a set of related information using indecies?', ['object', 'array', 'function'], 'array');
+    const card3 = new Card(1, 'What allows you to use a block of code multiple times?', ['object', 'array', 'function'], 'function');
+    const deck = new Deck([card1, card2, card3]);
+    expect(deck.cards).to.be.deep.equal([card1, card2, card3]);
+  });
+
+  it.skip('should know how many cards are in the Deck', () => {
+    const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const card2 = new Card(1, 'What allows you to define a set of related information using indecies?', ['object', 'array', 'function'], 'array');
+    const card3 = new Card(1, 'What allows you to use a block of code multiple times?', ['object', 'array', 'function'], 'function');
+    const deck = new Deck([card1, card2, card3]);
+    expect(deck.countCards).to.be.equal(3)
+  })
+});
