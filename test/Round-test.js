f4fb0f351ffcad0613f975edeecafa4303f83c4a
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-02-02 16:50:58 -0700

    Fill out logic for taketurn method test

--- test/Round-test.js
+++ test/Round-test.js
@@ -2,6 +2,7 @@ const expect = require('chai').expect;
 const Round = require('../src/Round');
 const Card = require('../src/Card');
 const Deck = require('../src/Deck');
+const Turn = require('../src/Turn');
 
 describe('Round', () => {
 
@@ -31,7 +32,7 @@ describe('Round', () => {
   });
 
   it.skip('should be able to return the current card being played', () => {
-    expect(round.returnCurrentCard()).to.deep.equal(deck.cards[0]);
+    expect(round.returnCurrentCard()).to.deep.equal(round.deck.cards[0]);
   });
 
   it.skip('should have a turn count thats 0 by default', () => {
@@ -44,24 +45,67 @@ describe('Round', () => {
 
   describe('When a guess is made', () => {
 
-    it.skip('a new Turn is created', () => {
+    beforeEach(() => {
+      const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+      const card2 = new Card(2, 'What allows you to define a set of related information using indecies?', ['object', 'array', 'function'], 'array');
+      const card3 = new Card(3, 'What allows you to use a block of code multiple times?', ['object', 'array', 'function'], 'function');
+      const cards = [card1, card2, card3];
+      const deck = new Deck(cards);
+      const round = new Round('object', deck);
+    });
 
+    it.skip('guess is evaluated using Turns as correct', () => {
+      let result = round.takeTurn('object');
+      expect(result).to.be.deep.equal('correct!');
+    });
+
+    it.skip('guess is evaluated using Turns as incorrect', () => {
+      let result = round.takeTurn('array');
+      expect(result).to.be.deep.equal('incorrect!');
+    });
+
+    it.skip('incorrect guesses are recorded', () => {
+      round.takeTurn('array');
+      expect(round.incorrectGuesses).to.deep.equal([1]);
     });
 
     it.skip('turns count is updated', () => {
-
+      round.takeTurn('object');
+      expect(round.turns).to.be.deep.equal(1);
+      round.takeTurn('object');
+      expect(round.turns).to.be.deep.equal(2);
     });
 
     it.skip('updates the current card', () => {
-
+      round.takeTurn('object');
+      expect(round.returnCurrentCard()).to.be.equal(round.deck.cards[1]);
     });
+  });
 
-    it.skip('guess is evaluated', () => {
+  it.skip('should be able to calculate the percentage of correct guesses', () => {
+    round.takeTurn('object');
+    round.takeTurn('array');
+    round.takeTurn('function');
+    expect(round.calculatePercentCorrect()).to.equal(100);
+  });
 
-    });
+  it.skip('should be able to calculate the percentage of other correct guesses', () => {
+    round.takeTurn('object');
+    round.takeTurn('object');
+    expect(round.calculatePercentCorrect()).to.equal(50);
+  });
 
-    it.skip('feedback is given', () => {
+  it.skip('should be able to end the round', () => {
+    round.takeTurn('object');
+    round.takeTurn('array');
+    round.takeTurn('function');
+    expect(round.endRound()).to.deep.equal('** Round over! ** You answered 100% of the questions correctly!');
+  });
 
-    });
+  it.skip('should be able to end a different round', () => {
+    round.takeTurn('object');
+    round.takeTurn('object');
+    round.takeTurn('function');
+    expect(round.endRound()).to.deep.equal('** Round over! ** You answered 66% of the questions correctly!');
   });
 });
