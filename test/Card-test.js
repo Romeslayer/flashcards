3a51312e7ec1ad3429f67122f85ec6bb50d3ed16
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-02-03 09:23:09 -0700

    Refactor to use variable

--- test/Card-test.js
+++ test/Card-test.js
@@ -19,18 +19,18 @@ describe('Card', function() {
   it('should store a question', function() {
     const cardData = data[0];
     const card = new Card(cardData);
-    expect(card.question).to.equal(data[0].question);
+    expect(card.question).to.equal(cardData.question);
   });
 
   it('should store a list of possible answers', function() {
     const cardData = data[0];
     const card = new Card(cardData);
-    expect(card.answers).to.deep.equal(data[0].answers);
+    expect(card.answers).to.deep.equal(cardData.answers);
   });
 
   it('should store the correct answer', function() {
     const cardData = data[0];
     const card = new Card(cardData);
-    expect(card.correctAnswer).to.equal(data[0].correctAnswer);
+    expect(card.correctAnswer).to.equal(cardData.correctAnswer);
   });
 });
