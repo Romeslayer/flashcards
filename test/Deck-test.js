6fcfa911f91952edfa569c852cf4439df9e5326d
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-02-03 16:27:00 -0700

    Refactor tests to use before each hook

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -5,31 +5,27 @@ const data = require('../src/data').prototypeData;
 
 describe('Deck', () => {
 
+  beforeEach(() => {
+    card1 = new Card(data[0]);
+    card2 = new Card(data[1]);
+    card3 = new Card(data[2]);
+    cards = [card1, card2, card3];
+    deck = new Deck(cards);
+  });
+
   it('should be a function', () => {
-    const deck = new Deck();
     expect(Deck).to.be.a('function');
   });
 
   it('should be an instance of Deck', () => {
-    const deck = new Deck();
     expect(deck).to.be.an.instanceof(Deck);
   });
 
   it('should hold Cards', () => {
-    const card1 = new Card(data[0]);
-    const card2 = new Card(data[1]);
-    const card3 = new Card(data[2]);
-    const cards = [card1, card2, card3];
-    const deck = new Deck(cards);
     expect(deck.cards).to.be.deep.equal(cards);
   });
 
   it('should know how many cards are in the Deck', () => {
-    const card1 = new Card(data[0]);
-    const card2 = new Card(data[1]);
-    const card3 = new Card(data[2]);
-    const cards = [card1, card2, card3];
-    const deck = new Deck(cards);
     expect(deck.countCards()).to.be.equal(deck.cards.length);
   })
 });
