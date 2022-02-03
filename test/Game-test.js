f4728fac5b60c2e616763c7c4eeb156345fa5971
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-02-02 22:51:27 -0700

    Create tests to check game instance

--- test/Game-test.js
+++ test/Game-test.js
@@ -4,4 +4,15 @@ const Card = require('../src/Card');
 const Deck = require('../src/Deck');
 const Round = require('../src/Round');
 const data = require('../src/data').prototypeData;
-console.log(data)
+
+describe('Game', () => {
+
+  it.skip('should be a function', () => {
+    expect(Game).to.be.a('function');
+  });
+
+  it.skip('should instantiate a Game', () => {
+    const game = new Game(data);
+    expect(game).to.be.an.instanceof(Game);
+  });
+});
