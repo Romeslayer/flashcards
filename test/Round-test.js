0b25aeb898016572eeabcd361653129d7e3c3d65
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-02-02 17:53:18 -0700

    Refactor to remove moisutre

--- test/Round-test.js
+++ test/Round-test.js
@@ -2,28 +2,25 @@ const expect = require('chai').expect;
 const Round = require('../src/Round');
 const Card = require('../src/Card');
 const Deck = require('../src/Deck');
-const Turn = require('../src/Turn');
 
 describe('Round', () => {
 
-  beforeEach(() => {
     const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     const card2 = new Card(2, 'What allows you to define a set of related information using indecies?', ['object', 'array', 'function'], 'array');
     const card3 = new Card(3, 'What allows you to use a block of code multiple times?', ['object', 'array', 'function'], 'function');
     const cards = [card1, card2, card3];
     const deck = new Deck(cards);
     const round = new Round('object', deck);
-  });
 
-  it.skip('should be a function', () => {
+  it('should be a function', () => {
     expect(Round).to.be.a('function');
   });
 
-  it.skip('should be an instance of Round', () => {
+  it('should be an instance of Round', () => {
     expect(round).to.be.an.instanceof(Round);
   });
 
-  it.skip('should store a guess', () => {
+  it('should store a guess', () => {
     expect(round.guess).to.be.equal('object');
   });
 
@@ -40,72 +37,72 @@ describe('Round', () => {
   });
 
   it.skip('should be able to store incorrect guesses', () => {
-      expect(round.incorrectGuesses).to.equal([]);
+    expect(round.incorrectGuesses).to.equal([]);
   });
 
   describe('When a guess is made', () => {
 
-    beforeEach(() => {
-      const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-      const card2 = new Card(2, 'What allows you to define a set of related information using indecies?', ['object', 'array', 'function'], 'array');
-      const card3 = new Card(3, 'What allows you to use a block of code multiple times?', ['object', 'array', 'function'], 'function');
-      const cards = [card1, card2, card3];
-      const deck = new Deck(cards);
-      const round = new Round('object', deck);
-    });
-
     it.skip('guess is evaluated using Turns as correct', () => {
-      let result = round.takeTurn('object');
+      let testRound = round;
+      let result = testRound.takeTurn('object');
       expect(result).to.be.deep.equal('correct!');
     });
 
     it.skip('guess is evaluated using Turns as incorrect', () => {
-      let result = round.takeTurn('array');
+      let testRound = round;
+      let result = testRound.takeTurn('array');
       expect(result).to.be.deep.equal('incorrect!');
     });
 
     it.skip('incorrect guesses are recorded', () => {
-      round.takeTurn('array');
-      expect(round.incorrectGuesses).to.deep.equal([1]);
+      let testRound = round;
+      testRound.takeTurn('array');
+      expect(testRound.incorrectGuesses).to.deep.equal([1]);
     });
 
     it.skip('turns count is updated', () => {
-      round.takeTurn('object');
-      expect(round.turns).to.be.deep.equal(1);
-      round.takeTurn('object');
-      expect(round.turns).to.be.deep.equal(2);
+      let testRound = round;
+      testRound.takeTurn('object');
+      expect(testRound.turns).to.be.deep.equal(1);
+      testRound.takeTurn('object');
+      expect(testRound.turns).to.be.deep.equal(2);
     });
 
     it.skip('updates the current card', () => {
-      round.takeTurn('object');
-      expect(round.returnCurrentCard()).to.be.equal(round.deck.cards[1]);
+      let testRound = round;
+      testRound.takeTurn('object');
+      expect(testRound.returnCurrentCard()).to.be.equal(testRound.deck.cards[1]);
     });
   });
 
   it.skip('should be able to calculate the percentage of correct guesses', () => {
-    round.takeTurn('object');
-    round.takeTurn('array');
-    round.takeTurn('function');
-    expect(round.calculatePercentCorrect()).to.equal(100);
+    let testRound = round;
+    testRound.takeTurn('object');
+    testRound.takeTurn('array');
+    testRound.takeTurn('function');
+    expect(testRound.calculatePercentCorrect()).to.equal(100);
   });
 
   it.skip('should be able to calculate the percentage of other correct guesses', () => {
-    round.takeTurn('object');
-    round.takeTurn('object');
-    expect(round.calculatePercentCorrect()).to.equal(50);
+    let testRound = round;
+    testRound.takeTurn('object');
+    testRound.takeTurn('object');
+    expect(testRound.calculatePercentCorrect()).to.equal(50);
   });
 
   it.skip('should be able to end the round', () => {
-    round.takeTurn('object');
-    round.takeTurn('array');
-    round.takeTurn('function');
-    expect(round.endRound()).to.deep.equal('** Round over! ** You answered 100% of the questions correctly!');
+    let testRound = round;
+    testRound.takeTurn('object');
+    testRound.takeTurn('array');
+    testRound.takeTurn('function');
+    expect(testRound.endRound()).to.deep.equal('** Round over! ** You answered 100% of the questions correctly!');
   });
 
   it.skip('should be able to end a different round', () => {
-    round.takeTurn('object');
-    round.takeTurn('object');
-    round.takeTurn('function');
-    expect(round.endRound()).to.deep.equal('** Round over! ** You answered 66% of the questions correctly!');
+    let testRound = round;
+    testRound.takeTurn('object');
+    testRound.takeTurn('object');
+    testRound.takeTurn('function');
+    expect(testRound.endRound()).to.deep.equal('** Round over! ** You answered 66% of the questions correctly!');
   });
 });
