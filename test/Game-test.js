8681ef6d4ec18a4fc433240d801fe4098eaec9d0
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-02-02 22:40:53 -0700

    Create setup variables

---
+++ test/Game-test.js
@@ -0,0 +1,7 @@
+const expect = require('chai').expect;
+const Game = require('../src/Game');
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
+const data = require('../src/data').prototypeData;
+console.log(data)
