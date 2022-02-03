3f943210f483e2eaf1e82af6cb41948d94c91acf
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-02-03 15:42:31 -0700

    Refactor test to use beforeEach

--- test/Game-test.js
+++ test/Game-test.js
@@ -7,50 +7,19 @@ const data = require('../src/data').prototypeData;
 
 describe('Game', () => {
 
+  beforeEach(() => {
+    game = new Game();
+  });
+
   it('should be a function', () => {
     expect(Game).to.be.a('function');
   });
 
   it('should instantiate a Game', () => {
-    const game = new Game();
     expect(game).to.be.an.instanceof(Game);
   });
 
-  it.skip('should be able to keep track of the current round', () => {
-    const game = new Game();
-    expect(game.currentRound).to.be.equal({})
-  })
-
-  describe('start()', () => {
-
-    it.skip('creates Cards and puts them in a Deck', () => {
-      const game = new Game();
-      game.start();
-      expect(game.deck).to.be.an.instanceof(Deck);
-      expect(game.deck.cards[0]).to.be.an.instanceof(Card);
-    });
-
-    it.skip('creates Cards from data', () => {
-      const game = new Game();
-      game.start();
-      const testCard = new Card(data[0]);
-      expect(game.deck.cards[0]).to.be.deep.equal(testCard);
-      expect(game.deck.cards.length).to.be.equal(testCard);
-    })
-  });
-
-  it.skip('should create an instance of the round', () => {
-    const game = new Game();
-    game.start();
-    expect(game.currentRound).to.be.an.instanceof(Round);
-  });
-
-  it.skip('should create the correct instance of the round', () => {
-    const game = new Game();
-    game.start()
-    const testCard = new Card(data[0]);
-    const testDeck = new Deck([testCard]);
-    const testRound = new Round(testDeck);
-    expect(game.currentRound).to.be.deep.equal(testRound);
+  it('should be able to keep track of the current round', () => {
+    expect(game.currentRound).to.be.deep.equal({})
   });
 });
