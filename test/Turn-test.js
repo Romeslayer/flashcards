99d95c08e5e5bb1bfc647a6089c5cc5f0596f79a
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-02-03 16:28:32 -0700

    Refactor tests to use before each

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -1,8 +1,14 @@
 const expect = require('chai').expect;
 const Card = require('../src/Card');
 const Turn = require('../src/Turn');
+const data = require('../src/data').prototypeData;
 
 describe('Turn', function() {
+
+  beforeEach(() => {
+    card = new Card(data[0])
+    turn = new Turn(card.correctAnswer, card)
+  });
 
   it('should be a function', function() {
     const turn = new Turn();
@@ -10,36 +16,27 @@ describe('Turn', function() {
   });
 
   it('should be an instance of Turn', function() {
-    const turn = new Turn();
     expect(turn).to.be.an.instanceof(Turn);
   });
 
   it('should be able return the user\'s guess', function() {
-    const card = new Card();
-    const turn = new Turn('opera', card);
-    expect(turn.returnGuess()).to.equal('opera');
+    expect(turn.returnGuess()).to.equal(card.correctAnswer);
   });
 
   it('should be able return the card', function() {
-    const card = new Card();
-    const turn = new Turn('opera', card);
     expect(turn.returnCard()).to.equal(card);
   });
 
   it('should be able to check if user\'s guess is correct', function() {
-    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    const turn = new Turn('object', card);
     expect(turn.evaluateGuess()).to.equal(true);
   });
 
   it('should be able to check if user\'s guess is incorrect', function() {
-    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     const turn = new Turn('function', card)
     expect(turn.evaluateGuess()).to.equal(false);
   });
 
   it('should be able to say if guess is correct or not', function() {
-    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     const turn1 = new Turn('function', card);
     expect(turn1.giveFeedback()).to.equal('incorrect!');
     const turn2 = new Turn('object', card);
