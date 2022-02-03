6cd97a7613c00d48acb99058cf025ceef9829c48
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-02-03 15:48:47 -0700

    Refactor to set up test beforehand

--- test/Card-test.js
+++ test/Card-test.js
@@ -5,32 +5,28 @@ const Card = require('../src/Card');
 
 describe('Card', function() {
 
+  beforeEach(() => {
+    cardData = data[0];
+    card = new Card(cardData);
+  });
+
   it('should be a function', function() {
-    const card = new Card(data);
     expect(Card).to.be.a('function');
   });
 
   it('should be an instance of Card', function() {
-    const cardData = data[0];
-    const card = new Card(cardData);
     expect(card).to.be.an.instanceof(Card);
   });
 
   it('should store a question', function() {
-    const cardData = data[0];
-    const card = new Card(cardData);
     expect(card.question).to.equal(cardData.question);
   });
 
   it('should store a list of possible answers', function() {
-    const cardData = data[0];
-    const card = new Card(cardData);
     expect(card.answers).to.deep.equal(cardData.answers);
   });
 
   it('should store the correct answer', function() {
-    const cardData = data[0];
-    const card = new Card(cardData);
     expect(card.correctAnswer).to.equal(cardData.correctAnswer);
   });
 });
