8e46102ae9cafc2e3ee43b7f02bcb489f4b328de
DeNaje Ferguson <denaje.ferguson@gmail.com>
2022-02-03 13:57:33 -0700

    Create test to check curretnRound

--- test/Game-test.js
+++ test/Game-test.js
@@ -7,26 +7,31 @@ const data = require('../src/data').prototypeData;
 
 describe('Game', () => {
 
-  it.skip('should be a function', () => {
+  it('should be a function', () => {
     expect(Game).to.be.a('function');
   });
 
-  it.skip('should instantiate a Game', () => {
-    const game = new Game(data);
+  it('should instantiate a Game', () => {
+    const game = new Game();
     expect(game).to.be.an.instanceof(Game);
   });
+
+  it.skip('should be able to keep track of the current round', () => {
+    const game = new Game();
+    expect(game.currentRound).to.be.equal({})
+  })
 
   describe('start()', () => {
 
     it.skip('creates Cards and puts them in a Deck', () => {
-      const game = new Game(data);
+      const game = new Game();
       game.start();
       expect(game.deck).to.be.an.instanceof(Deck);
       expect(game.deck.cards[0]).to.be.an.instanceof(Card);
     });
 
     it.skip('creates Cards from data', () => {
-      const game = new Game(data);
+      const game = new Game();
       game.start();
       const testCard = new Card(data[0]);
       expect(game.deck.cards[0]).to.be.deep.equal(testCard);
@@ -35,13 +40,13 @@ describe('Game', () => {
   });
 
   it.skip('should create an instance of the round', () => {
-    const game = new Game(data);
+    const game = new Game();
     game.start();
     expect(game.currentRound).to.be.an.instanceof(Round);
   });
 
   it.skip('should create the correct instance of the round', () => {
-    const game = new Game(data);
+    const game = new Game();
     game.start()
     const testCard = new Card(data[0]);
     const testDeck = new Deck([testCard]);
